0d2bbbec8e88c20fac1af24c33aa2282d1101e97
jailson <jailson491@gmail.com>
2022-03-09 16:42:06 -0300

    V2

---
+++ C#/Santader.UserControl/Santader.UserControl/Santader.UserControl/Controllers/TokenController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Santader.UserControl.Models;
+using Santader.UserControl.Repositories;
+using Santader.UserControl.Services;
+using Serilog;
+
+namespace Santader.UserControl.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+
+        private readonly IUserRepository repos;
+        public TokenController(IUserRepository _repos)
+        {
+            repos = _repos;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Get(Login login)
+        {
+            try
+            {
+                Log.Information("Iniciando Post Token");
+
+                User _users = await repos.FindUserAsync(login.UserName, login.Password);
+                if (_users == null)
+                {
+                    Log.ForContext("Action", $"Token").Information($"API Token GET Invalid User Name or password.");
+                    return Forbid();
+                }
+                JsonWebToken tokeService = TokenServices.GenarateToken(_users);
+                Log.ForContext("Action", $"Token").Information($"API chamada por {tokeService}.");
+
+                return Ok(tokeService);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.Message);
+                return BadRequest(ex.Message);
+            }
+            finally
+            {
+                Log.ForContext("Action", $"Token").Information($"API Retorno 200");
+            }
+        }
+    }
+}
+
--- PowerShell/src/UserModule.psm1
+++ PowerShell/src/UserModule.psm1
@@ -1,7 +1,7 @@
 function GetUser {
     param (
         [string] $name,
-        [string] $_password
+        [string] $pass
     )        
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $urlBase = "https://localhost:7205/api/"
@@ -10,7 +10,7 @@ function GetUser {
 
     $retornoLogin = Invoke-RestMethod  -Uri ($urlBase + "Token") -Method POST -ContentType "application/json" -Body $credenciais
     
-    if ($retornoLogin){
+    if ($retornoLogin) {
 
         $url_API_Users = ($urlBase + "Users/V1/ListUser")
 
